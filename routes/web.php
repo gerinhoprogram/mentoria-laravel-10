ff02f7c3063fcea2e9f0158911ae2a47a948f39a
gerinhoprogram <rogerio_furquim@hotmail.com>
2023-11-12 21:01:38 -0300

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/mentoria', function(){
+    return 'ola';
+});
+
+// http://localhost:8989/produtos/mais alguma coisa
+Route::prefix('produtos')->group(function(){
+    //controller e função
+    Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
+});
+
